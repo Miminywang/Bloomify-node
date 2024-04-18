cf9d3b12bac16f1bda6a576f4db002378924698d
simpleme37 <simpleme307@gmail.com>
2024-04-18 10:41:04 +0800

    update course and member data

--- routes/courses.js
+++ routes/courses.js
@@ -7,11 +7,38 @@ import { getIdParam } from '#db-helpers/db-tool.js'
 // 資料庫使用
 import sequelize from '#configs/db.js'
 
-const { Course, Course_Image, Course_Category } = sequelize.models
+const {
+  Course,
+  Course_Image,
+  Course_Category,
+  Share_Store,
+  Course_News,
+  Course_Datetime,
+  Course_Review,
+  Share_Member,
+} = sequelize.models
 
-// 外鍵 - 圖片資料表 & 分類資料表 定義
+// 外鍵 - 圖片資料表定義
 Course.hasMany(Course_Image, { foreignKey: 'course_id', as: 'images' })
 Course_Image.belongsTo(Course, { foreignKey: 'course_id' })
+// 外鍵 - 商家資料表定義
+Share_Store.hasMany(Course, { foreignKey: 'store_id' })
+Course.belongsTo(Share_Store, { foreignKey: 'store_id', as: 'store' })
+// 外鍵 - 最新消息資料表定義
+Course.hasMany(Course_News, { foreignKey: 'course_id', as: 'news' })
+Course_News.belongsTo(Course, { foreignKey: 'course_id' })
+// 外鍵 - 上課日期資料表定義
+Course.hasMany(Course_Datetime, { foreignKey: 'course_id', as: 'datetimes' })
+Course_Datetime.belongsTo(Course, { foreignKey: 'course_id' })
+// 外鍵 - 評價資料表定義
+Course.hasMany(Course_Review, { foreignKey: 'course_id', as: 'reviews' })
+Course_Review.belongsTo(Course, { foreignKey: 'course_id' })
+// 外鍵 - 會員資料表定義
+Share_Member.hasMany(Course_Review, { foreignKey: 'course_id' })
+Course_Review.belongsTo(Share_Member, {
+  foreignKey: 'member_id',
+  as: 'member',
+})
 
 // GET - 得到所有課程
 router.get('/', async function (req, res) {
@@ -19,7 +46,7 @@ router.get('/', async function (req, res) {
     const courses = await Course.findAll({
       include: [
         {
-          model: Course_Image,
+          model: Course_Image, // 引入圖片資料表
           as: 'images', // 確保在模型定義中使用這個別名
           attributes: ['id', 'path', 'is_main'],
         },
@@ -114,9 +141,42 @@ router.get('/:id', async function (req, res) {
   //   return res.json({ status: 'error', message: '存取會員資料失敗' })
   // }
 
-  // 這只有一張表
   const course = await Course.findByPk(id, {
-    raw: true, // 只需要資料表中資料
+    include: [
+      {
+        model: Course_Image,
+        as: 'images', // 確保在模型定義中使用這個別名
+        attributes: ['id', 'path', 'is_main'],
+      },
+      {
+        model: Share_Store,
+        as: 'store',
+        attributes: ['store_id', 'store_name', 'store_address', 'store_tel'],
+      },
+      {
+        model: Course_News,
+        as: 'news',
+        attributes: ['id', 'title', 'content', 'created_at'],
+      },
+      {
+        model: Course_Datetime,
+        as: 'datetimes',
+        attributes: ['id', 'period', 'date', 'start_time', 'end_time'],
+      },
+      {
+        model: Course_Review,
+        as: 'reviews',
+        attributes: ['member_id', 'rating', 'comment', 'created_at'],
+        include: [
+          {
+            model: Share_Member,
+            as: 'member',
+            attributes: ['name'],
+          },
+        ],
+      },
+    ],
+    nest: true,
   })
 
   // 不回傳密碼
